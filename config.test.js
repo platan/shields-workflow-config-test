777527525264f07ec8f5be16fc11fc5f2af2b14b
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-08-19 16:50:41 +0200

    Read probot config from file

--- config.test.js
+++ config.test.js
@@ -1,32 +1,20 @@
-const {createRobot} = require('probot')
+const { createRobot } = require('probot')
 const app = require('probot-workflow')
+const fs = require('fs');
 
 describe('app', () => {
   let robot
   let github
-  const configuration = `
-  on('pull_request.closed')
-  .filter(context => context.payload.pull_request.merged)
-  .filter(
-    context =>
-      context.payload.pull_request.head.ref.slice(0, 11) !== 'dependabot/'
-  )
-  .filter(context => context.payload.pull_request.base.ref === 'master')
-  .comment(\`This pull request was merged to [{{ pull_request.base.ref }}]({{ repository.html_url }}/tree/{{ pull_request.base.ref }}) branch. Now this change is waiting for deployment. 
-Deploys usually happen every few weeks. After deployment changes are copied to [gh-pages]({{ repository.html_url }}/tree/gh-pages) branch. 
 
-This badge displays deployment status:
-![](https://img.shields.io/github/commit-status/{{ repository.full_name }}/gh-pages/{{ pull_request.merge_commit_sha }}.svg?label=deploy%20status)\`)
-    `; 
-
-  const configure = async (content, payload) => {
-    github.repos.getContent.mockImplementation(params => Promise.resolve({
-      data: {
-        content: Buffer
-          .from(content)
-          .toString('base64')
-      }
-    }))
+  const configure = async (payload) => {
+    github.repos.getContent.mockImplementation(params => {
+      const config = fs.readFileSync(params.path);
+      return Promise.resolve({
+        data: {
+          content: config
+        }
+      })
+    })
     robot.auth = () => Promise.resolve(github)
     await robot.receive({ event: 'pull_request', payload: require(payload) })
   }
@@ -40,20 +28,20 @@ This badge displays deployment status:
         getContent: jest.fn().mockReturnValue(Promise.resolve({}))
       },
       issues: {
-        createComment: jest.fn(),
-        edit: jest.fn()
+        createComment: jest.fn()
+        // TODO mock other functions, e.g edit
       }
     }
   })
 
   describe('create a comment after closing a pr', () => {
     it('accept', async () => {
-      await configure(configuration, './fixtures/pr-closed.json')
+      await configure('./fixtures/pr-closed.json')
       expect(github.issues.createComment).toHaveBeenCalled()
     })
 
     it('skip pr made by dependabot', async () => {
-      await configure(configuration, './fixtures/pr-closed-by-dependabot.json')
+      await configure('./fixtures/pr-closed-by-dependabot.json')
       expect(github.issues.createComment).not.toHaveBeenCalled()
     })
   })
